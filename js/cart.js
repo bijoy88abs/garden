6aa0795e3d67d5b3f498abca4c137cecd8882af0
bijoy88abs <bijoyduttajis88@gmail.com>
2024-09-04 17:37:26 +0530

    fix added

--- js/cart.js
+++ js/cart.js
@@ -10,37 +10,37 @@ $(document).ready(function () {
             itemId: 'item-2',
             title: 'Attractive Peperomia',
             price: '$100.00',
-            img: 'images/bestseller/img1.png'
+            img: 'images/bestseller/img2.png'
         },
         {
             itemId: 'item-3',
             title: 'Acoma Crape Myrtle',
             price: '$200.00',
-            img: 'images/bestseller/img2.png'
+            img: 'images/bestseller/img3.png'
         },
         {
             itemId: 'item-4',
             title: 'Black-Eyed Susan Vine',
             price: '$120.00',
-            img: 'images/bestseller/img3.png'
+            img: 'images/bestseller/img4.png'
         },
         {
             itemId: 'item-5',
             title: 'Acoma Crape Myrtle',
             price: '$170.00',
-            img: 'images/bestseller/img4.png'
+            img: 'images/bestseller/img5.png'
         },
         {
             itemId: 'item-6',
             title: 'Good Luck Plants',
             price: '$70.00',
-            img: 'images/bestseller/img5.png'
+            img: 'images/bestseller/img6.png'
         },
         {
             itemId: 'item-7',
             title: 'house Parlor Palm',
             price: '$130.00',
-            img: 'images/bestseller/img6.png'
+            img: 'images/bestseller/img7.png'
         },
     ];
 
