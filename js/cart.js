6da1800c2f22c52dce3dd6b43b5e8c85ae028dad
bijoy88abs <bijoyduttajis88@gmail.com>
2024-09-07 12:51:57 +0530

    fix added

--- js/cart.js
+++ js/cart.js
@@ -4,60 +4,159 @@ $(document).ready(function () {
             itemId: 'item-1',
             title: 'Chinese Quince',
             price: '$200.00',
-            img: 'images/bestseller/img1.png'
+            img: 'images/bestseller/img1.png',
+            addedToCart: false,
         },
         {
             itemId: 'item-2',
             title: 'Attractive Peperomia',
             price: '$100.00',
-            img: 'images/bestseller/img2.png'
+            img: 'images/bestseller/img2.png',
+            addedToCart: false,
         },
         {
             itemId: 'item-3',
             title: 'Acoma Crape Myrtle',
             price: '$200.00',
-            img: 'images/bestseller/img3.png'
+            img: 'images/bestseller/img3.png',
+            addedToCart: false,
         },
         {
             itemId: 'item-4',
             title: 'Black-Eyed Susan Vine',
             price: '$120.00',
-            img: 'images/bestseller/img4.png'
+            img: 'images/bestseller/img4.png',
+            addedToCart: false,
         },
         {
             itemId: 'item-5',
             title: 'Acoma Crape Myrtle',
             price: '$170.00',
-            img: 'images/bestseller/img5.png'
+            img: 'images/bestseller/img5.png',
+            addedToCart: false,
         },
         {
             itemId: 'item-6',
             title: 'Good Luck Plants',
             price: '$70.00',
-            img: 'images/bestseller/img6.png'
+            img: 'images/bestseller/img6.png',
+            addedToCart: false,
         },
         {
             itemId: 'item-7',
             title: 'house Parlor Palm',
             price: '$130.00',
-            img: 'images/bestseller/img7.png'
+            img: 'images/bestseller/img7.png',
+            addedToCart: false,
         },
     ];
 
-    $(".add-to-cart-btn").click(function (e) {
+    let retrievedObject = JSON.parse(localStorage.getItem('cartObject'));
+    if (!retrievedObject) {
+        localStorage.setItem('cartObject', JSON.stringify(cartList));
+    }
+    checkCartItems();
+
+    $(".item-cart-icons").click(function (e) {
+        let selectedCartClass = $(this).attr('alt');
+        let selectedIdSplit = selectedCartClass.split("-");
+        let selectedId = `${selectedIdSplit[0]}-${selectedIdSplit[1]}`;
+        if (selectedId) {
+            let findObj = retrievedObject.find((i) => i.itemId == selectedId);
+            if (findObj) {
+                const newList = [];
+
+                retrievedObject.forEach((e) => {
+                    if (e.itemId == selectedId) {
+                        newList.push({
+                            itemId: e.itemId,
+                            title: e.title,
+                            price: e.price,
+                            img: e.img,
+                            addedToCart: true,
+                        });
+                    } else {
+                        newList.push(e);
+                    }
+                });
+                $('#addedToCartModal').modal('show');
+                localStorage.removeItem('cartObject');
+                localStorage.setItem('cartObject', JSON.stringify(newList));
+                checkCartItems();
+            }
+        }
+    });
+
+    $(".item-cart-icons-del").click(function (e) {
+        let selectedCartClass = $(this).attr('alt');
+        let selectedIdSplit = selectedCartClass.split("-");
+        let selectedId = `${selectedIdSplit[0]}-${selectedIdSplit[1]}`;
+        console.log(selectedId)
+        if (selectedId) {
+            let findObj = retrievedObject.find((i) => i.itemId == selectedId);
+            if (findObj) {
+                const newList = [];
+                retrievedObject.forEach((e) => {
+                    if (e.itemId == selectedId) {
+                        newList.push({
+                            itemId: e.itemId,
+                            title: e.title,
+                            price: e.price,
+                            img: e.img,
+                            addedToCart: false,
+                        });
+                    } else {
+                        newList.push(e);
+                    }
+                });
+                localStorage.removeItem('cartObject');
+                localStorage.setItem('cartObject', JSON.stringify(newList));
+                checkCartItems();
+            }
+        }
+    });
+
+    $(".add-to-order-btn").click(function (e) {
         $('#cart-item-title').html('');
         $('#cart-item-price').html('');
         let x = $(this).attr('alt');
-        console.log('===', x)
-        cartList.forEach((e) => {
+        retrievedObject.forEach((e) => {
             if (e.itemId == x) {
                 $('#cart-item-title').html(e.title);
                 $('#cart-item-price').html(e.price);
                 $('#cart-item-img').attr('src', e.img);
                 $('#cart-item-img2').attr('src', e.img);
             }
-        })
+        });
+    });
 
-    })
+    function checkCartItems() {
+        setTimeout(() => {
+            retrievedObject = JSON.parse(localStorage.getItem('cartObject'));
+            let cartCount = 0;
+            let cartPrice = 0.00;
+            
+            retrievedObject.forEach((e) => {
+                const createIdLabel = `${e.itemId}-cart-label`;
+                const createIdCart = `${e.itemId}-cart-icon`;
+                const createIdCartDel = `${e.itemId}-cart-icon-del`;
+                $("." + createIdCart).hide();
+                $("." + createIdCartDel).hide();
+                if (e.addedToCart) {
+                    cartCount++;
+                    cartPrice = parseFloat(cartPrice) + parseFloat(e.price.substring(1));
+                    $("." + createIdLabel).html('Item added');
+                    $("." + createIdLabel).addClass('cart-active');
+                    $("." + createIdCartDel).show();
+                } else {
+                    $("." + createIdLabel).html('Add to cart');
+                    $("." + createIdCart).show();
+                    $("." + createIdLabel).removeClass('cart-active');
+                }
+            });
+            $(".cart-count").html(cartCount);
+            $(".cart-value").html(`$${cartPrice}.00`);
+        }, 100);
+    }
 
 });
